4bc84751eaab934700ec549890767d294d70d0e5
Omega Dziko F. <megaoy@yahoo.com>
2024-05-02 21:45:17 -0400

    phase one complete

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,23 @@
-module.exports = function reverseString(string) {
-  // Your code here 
-};
+
+reverseString = (string) => {
+
+  return string.split('').reverse().join('');
+  
+}
+// function reverseString(string) {
+
+//   let strArr = string.split('');
+
+//   let backwardWord = [];
+
+//   for (let i = strArr.length - 1; i >= 0; i--) {
+
+//    backwardWord.push(strArr[i]);
+
+//   }
+
+//   return backwardWord.join('');
+
+// };
+
+module.exports = reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,11 @@
-// Your code here 
+const chai = require("chai");
+const expect = chai.expect;
+
+const reverseString  = require("../problems/reverse-string");
+
+describe("reverseString", function () {
+    it('should return the reversed output', function () {
+        const versedString = reverseString("fun");
+        expect(versedString).to.equal("nuf");
+    });
+});
